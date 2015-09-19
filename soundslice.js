65a9da6b3a1b09e6b60a77b131fc80462648fe50
Matias Insaurralde <matias@insaurral.de>
2015-09-19 01:20:02 -0400

    update

--- soundslice.js
+++ soundslice.js
@@ -1,11 +1,8 @@
 angular.module( 'soundslice', []).
   directive( 'score', function() {
     return {
-      restrict: 'E',
       link: function( scope, element, attrs ) {
-
-        /*
-
+/*
           notation options:
            show_tab = 0
            show_staff = 0
@@ -23,11 +20,13 @@ angular.module( 'soundslice', []).
 
           other options:
             fretboard = 1
+*/
 
-        */
+        attrs.$observe( 'id', function( v ) {
+          var html = '<iframe src="https://www.soundslice.com/scores/' + attrs.id + '/embed/" width="100%" height="500" frameBorder="0" allowfullscreen></iframe>';
+          element.html( html );
+        });
 
-        var html = '<iframe src="https://www.soundslice.com/scores/' + attrs.id + '/embed/" width="100%" height="500" frameBorder="0" allowfullscreen></iframe>';
-        element.html( html );
       }
     };
   });
