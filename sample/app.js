eef3011f22e39b2d2447fff2f95c24f7ad0a2786
Matias Insaurralde <matias@insaurral.de>
2016-04-21 23:17:49 -0400

    Sample update

--- sample/app.js
+++ sample/app.js
@@ -1,5 +1,10 @@
 angular.module( 'musicApp', [ 'soundslice' ] )
-  .controller( 'scoreController', function() {
+  .controller( 'scoreController', function($rootScope) {
     var self = this;
+
     self.id = 12802;
+
+    $rootScope.$on( 'soundsliceEvent', function( e, ssEvent) {
+      console.log( 'Receiving event', ssEvent )
+    })
   });
